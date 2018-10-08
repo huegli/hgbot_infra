e0898c6be3e2389cab4a3e9a1036b0de490c964d
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2018-10-08 17:19:35 +0000

    BT pairing on exception

--- scripts/bt_joy.py
+++ scripts/bt_joy.py
@@ -7,10 +7,13 @@ bt.expect('#')
 bt.sendline('devices')
 bt.expect('#')
 try:
-    bt.expect('Dvice [A-F0-9:]+ Wireless')
+    bt.expect('Dvice [A-F0-9:]+ Wireless', timeout=10)
 except pexpect.exceptions.TIMEOUT:
     print("No wireless controller paired") 
-print("Wireless controller found")
+else:
+    print("Wireless controller found")
+    exit(0)
+print("Attempting to pair ...")
 #print bt.before
 #print bt.after
 
