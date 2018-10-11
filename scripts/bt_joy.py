47633353da65337a07289f17e5e43e054f9a886a
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2018-10-11 12:36:13 +0800

    Node launch throuch launch file

--- scripts/bt_joy.py
+++ scripts/bt_joy.py
@@ -2,57 +2,71 @@
 
 import pexpect
 import re
+
 import roslaunch
+import rospy
 
-mac_re = re.compile('[A-F0-9:]+')
 
-bt = pexpect.spawn('bluetoothctl')
-bt.expect('#')
-bt.sendline('devices')
-bt.expect('#')
-try:
-    bt.expect('Device [A-F0-9:]+ Wireless', timeout=10)
-except pexpect.exceptions.TIMEOUT:
-    print("No wireless controller paired") 
-else:
-    print("Wireless controller found")
-    exit(0)
-print("Attempting to pair ...")
-bt.sendline('agent on')
-bt.expect('#')
-bt.sendline('default-agent')
-bt.expect('#')
-bt.sendline('scan on')
-try:
-    bt.expect('[A-F0-9:]+ Wireless', timeout=300)
-except pexpect.exceptions.TIMEOUT:
-    print("No pairing attempt detected, quiting")
-    exit(1)
-m = mac_re.match(bt.after)
-if not m:
-    print("No valid MAC address detected")
-    exit(1)
-mac = m.group(0)
-print("Found {}".format(mac))
-bt.send('pair ')
-bt.sendline(mac)
-bt.expect('#')
-bt.send('trust ')
-bt.sendline(mac)
-bt.expect('#')
-print("Paired & trusted wireless controller")
-bt.sendline('exit')
+def pair_controller():
+    "Set up BT to scan for and pair with a wireless controller"
 
-package = 'joy'
-executable = 'joy_node'
-joy_node = roslaunch.core.Node(package, executable)
+    mac_re = re.compile('[A-F0-9:]+')
 
-package = 'teleop_twist_joy'
-executable = 'teleop_node'
-tele_node = roslaunch.core.Node(package, executable)
+    print("Looking for existing wireless controller")
+    bt = pexpect.spawn('bluetoothctl')
+    bt.expect('#')
+    bt.sendline('devices')
+    bt.expect('#')
 
-launch = roslaunch.scriptapi.ROSLaunch()
-launch.start()
+    try:
+        bt.expect('Device [A-F0-9:]+ Wireless', timeout=10)
+    except pexpect.exceptions.TIMEOUT:
+        print("No wireless controller paired")
+    else:
+        print("Wireless controller found")
+        return
 
-joy_process = launch.launch(joy_node)
-tele_process = launch.launch(tele_node)
+    print("Attempting to pair ...")
+    bt.sendline('agent on')
+    bt.expect('#')
+    bt.sendline('default-agent')
+    bt.expect('#')
+    bt.sendline('scan on')
+
+    try:
+        bt.expect('[A-F0-9:]+ Wireless', timeout=300)
+    except pexpect.exceptions.TIMEOUT:
+        print("No pairing attempt detected, quiting")
+        exit(1)
+
+    m = mac_re.match(bt.after)
+    if not m:
+        print("No valid MAC address detected")
+        exit(1)
+    mac = m.group(0)
+
+    print("Found {}".format(mac))
+    bt.send('pair ')
+    bt.sendline(mac)
+    bt.expect('#')
+    bt.send('trust ')
+    bt.sendline(mac)
+    bt.expect('#')
+    print("Paired & trusted wireless controller")
+    bt.sendline('exit')
+
+
+def launch_nodes():
+    "Call a ROS launch file on successful controller pairing"
+
+    rospy.init_node('bt_joy', anonymous=True)
+    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+    roslaunch.configure_logging(uuid)
+    launch = roslaunch.parent.ROSLaunchParent(
+            uuid,
+            ["/ros/hgbot_ws/src/hgbot_infra/launch/bt_joy.launch"])
+    launch.start()
+
+if __name__ == "__main__":
+    pair_controller()
+    launch_nodes()
