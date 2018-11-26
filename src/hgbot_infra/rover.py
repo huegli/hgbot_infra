6cd8fdff9267fe09ed1f715df593bb6b90c5784c
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2018-11-26 16:45:49 +0000

    Fleshing out rover class

--- src/hgbot_infra/rover.py
+++ src/hgbot_infra/rover.py
@@ -5,4 +5,18 @@ class Rover(object):
     def __init__(self, wheels = 4):
         pass
 
-    def 
+    def _limit_speed(self, speed):
+        return 0
+
+    def set_motor(self, motor, speed):
+        pass
+
+    def get_motor(self, motor):
+        return 0
+
+    def set_all(self, speed):
+        pass
+
+    def all_off(self):
+        pass
+        
